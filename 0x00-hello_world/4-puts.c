af7f2cdbbd36b9a8de4d1d6718e2afe390bb55bf
monsif-coder <monsiflamharml@gmail.com>
2023-07-15 16:45:53 -0700

    adhering to the betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-int main() {
-	char myfunc[] = "Programming is like building a multilingual puzzle";
+/**
+ * this puts function prints a string to the console
+ */
+int main(void)
+	
+{
+	char myfunc[] = "\"Programming is like building a multilingual puzzle";
 		puts(myfunc);
-		return 0;
+		return (0);
 
 }

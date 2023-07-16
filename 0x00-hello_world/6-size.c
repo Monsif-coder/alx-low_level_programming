f95aec7d776d42e3e1831f23e0e0557a6ad9284c
monsif-coder <monsiflamharml@gmail.com>
2023-07-16 16:49:00 -0700

    correcting 6-size.c for correct output

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,12 +4,12 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {	
 printf("the size of a char: %zu byte(s) \n", sizeof(char));
 printf("the size of a int: %d byte(s) \n", sizeof(int));
 printf("the size of a long int: %ld byte(s) \n", sizeof(long int));
 printf("the size of a long long int: %lld byte(s) \n", sizeof(long long int));
-printf("the size of a float: %f byte(s) \n", sizeof(float));
+printf("the size of a float: %zu byte(s) \n", sizeof(float));
 return (0);
 }

265c49370c45c95dc45bbc81742b1a5ee1949ea1
monsif-coder <monsiflamharml@gmail.com>
2023-07-16 16:29:57 -0700

    first try i ignored betty style

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{	
+printf("the size of a char: %zu byte(s) \n", sizeof(char));
+printf("the size of a int: %d byte(s) \n", sizeof(int));
+printf("the size of a long int: %ld byte(s) \n", sizeof(long int));
+printf("the size of a long long int: %lld byte(s) \n", sizeof(long long int));
+printf("the size of a float: %f byte(s) \n", sizeof(float));
+return (0);
+}

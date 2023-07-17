d33c5f91645bed13253e38c860eb4968baa968e8
monsif-coder <monsiflamharml@gmail.com>
2023-07-16 17:10:03 -0700

    had betty che3lat fiya chib

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
  * main - Entry point
  *
@@ -6,10 +6,10 @@
  */
 int main(void)
 {	
-printf("the size of a char: %zu byte(s) \n", sizeof(char));
-printf("the size of a int: %d byte(s) \n", sizeof(int));
-printf("the size of a long int: %ld byte(s) \n", sizeof(long int));
-printf("the size of a long long int: %lld byte(s) \n", sizeof(long long int));
-printf("the size of a float: %zu byte(s) \n", sizeof(float));
-return (0);
+	printf("the size of a char: %zu byte(s) \n", sizeof(char));
+	printf("the size of a int: %d byte(s) \n", sizeof(int));
+	printf("the size of a long int: %ld byte(s) \n", sizeof(long int));
+	printf("the size of a long long int: %zi byte(s) \n", sizeof(long long int));
+	printf("the size of a float: %zu byte(s) \n", sizeof(float));
+	return (0);
 }

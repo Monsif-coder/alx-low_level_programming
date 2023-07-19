1ca41762cf44ae15743f3c3a633d4d5a1de5d9ee
monsif-coder <monsiflamharml@gmail.com>
2023-07-19 07:16:43 -0700

    I fixed the issue of printing the whole number hamdullah

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,15 +14,15 @@ int main(void)
 	int lastdigit = n % 10;
 	if (lastdigit > 5)
 	{
-	printf(" is %d and is greater than 5", lastdigit);
+	printf("Last digit of %d  is %d and is greater than 5",n ,lastdigit);
 	}
 	else if (lastdigit == 0)
 	{
-		printf("is %d is 0 \n", lastdigit);
+		printf("Last digit of %d is %d and is 0 \n", n,lastdigit);
 		}
 	else if (lastdigit < 6)
 	{
-		printf("is %d and is less than 6 and not 0 \n", lastdigit);
+		printf("Last digit of %d is %d and is less than 6 and not 0 \n", n, lastdigit);
 	}
 	return (0);
 }

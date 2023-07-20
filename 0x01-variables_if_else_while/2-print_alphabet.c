bc445b127e9eccc2e8563cc65a59ea940477f628
monsif-coder <monsiflamharml@gmail.com>
2023-07-20 08:17:54 -0700

    pushing without compiling with betty

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int  alx = 'a';
+	 while (alx <= 'z') 
+	{
+	 putchar(alx);
+	alx++;
+	}putchar('\n');
+	return (0);
+}	

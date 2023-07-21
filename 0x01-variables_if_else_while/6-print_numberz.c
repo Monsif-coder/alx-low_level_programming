d35f726feb0c4b871dba19b4c67cc5f326b89863
monsif-coder <monsiflamharml@gmail.com>
2023-07-21 13:57:22 -0700

    print numbers from 0 to 9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main() {
+int mes=0;
+while (mes < 10)
+{
+
+    putchar(mes + '0');
+    mes++;
+}
+    putchar('\n');
+    return 0;
+}

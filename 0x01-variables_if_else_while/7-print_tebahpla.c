c070e3d4da946fd964a1be28e50ad9206af73471
monsif-coder <monsiflamharml@gmail.com>
2023-07-23 07:17:35 -0700

    betty not matched yet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	int hexi = 'z';
+
+	while (hexi >= 'a')
+	{ 
+	putchar(hexi);
+	}
+	putchar('\n');
+	return (0);
+}

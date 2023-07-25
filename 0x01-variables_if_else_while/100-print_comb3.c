a6232240d267f8e1f06c316c4d70fd3ab4c69384
monsif-coder <monsiflamharml@gmail.com>
2023-07-25 08:09:04 -0700

    last one

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int lbhja;
+    int ghousni;
+
+    for (ghousni = 0; ghousni <= 9; ghousni++) {
+        for (lbhja = 0; lbhja <= 9; lbhja++) {
+            if (ghousni < lbhja) {
+                putchar(ghousni + '0');
+                putchar(lbhja + '0');
+
+                if (ghousni != 8 || lbhja != 9) {
+                    putchar(' ');
+                    putchar(' ');
+                }
+            }
+        }
+    }
+
+    putchar('\n');
+
+    return 0;
+}

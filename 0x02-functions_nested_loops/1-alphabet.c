6cd0e979522393767cacfae4ffc0c7c9445278a7
monsif-coder <monsiflamharml@gmail.com>
2023-07-27 08:09:59 -0700

    betty not matched yet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void) 
+{
+	int x= 'a';
+	while (x <= 'z')
+{
+	putchar(x);
+	x++;
+}
+	putchar('\n');
+}

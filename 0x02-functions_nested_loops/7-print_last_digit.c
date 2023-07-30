8326b834f724f58e425e922dfad578a5eaeeaa5c
monsif-coder <monsiflamharml@gmail.com>
2023-07-30 14:04:10 -0700

    dealing with the compiling errors

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,24 +1,25 @@
 #include "main.h"
+
 /**
  * print_last_digit - prints the last digit of a number
- * @y: the int to extract the last digit from
+ * @x: the int to extract the last digit from
  * Return: value of the last digit
  */
-
 int print_last_digit(int x)
 {
-    int y = x % 10;
-    
-    
-    if (x < 0)
-	{
-		x = -x;
-	}
-    else if (y < 0)
-    
-	    y = -y;
-	_putchar('0'+y);
+    int y;
 
-    
-	return(y);
+    if (x < 0)
+    {
+        x = -x;
+        y = x % 10;
+    }
+    else
+    {
+        y = x % 10;
+    }
+
+    _putchar('0' + y);
+
+    return y;
 }

1116242a9757dfab6e3a4eee101947f1f17e53df
monsif <125797869+Monsif-coder@users.noreply.github.com>
2023-07-30 23:16:46 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,12 +2,11 @@
 
 
 	/**
-	 * times_table - prints the 9 times table, starting with 0
-	 */
+	* times_table - prints the 9 times table, starting with 0
+	*/
 	void times_table(void)
 	{
 		int i, j, k;
-
 
 		for (i = 0; i < 10; i++)
 		{

40f200bb14165ef842c135b984e5842c1f85f4f8
monsif-coder <monsiflamharml@gmail.com>
2023-08-05 15:52:48 -0700

    second try

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * swap_int  - swapes variables' value
+ *
+ * @x: its value gets swaped.
+ * @y: its value gets swaped as well.
+ */
+void swap_int(int *x, int *y)
+{
+	*y = 6;
+	*x = 7;
+}

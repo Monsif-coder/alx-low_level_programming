6b8ba02ab3d19cdb12c05056f3d4146f0b419c8e
monsif-coder <monsiflamharml@gmail.com>
2023-08-09 05:28:51 -0700

    first things first

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * _puts_recursion: it prints a string.
+ * @m: is a pointer.
+ */
+void _puts_recursion(char *m)
+{
+	if (*m == '\0')
+	{
+	putchar('\n');
+	return;
+	}
+
+	putchar(*m);
+	_puts_recursion(m + 1);
+}
